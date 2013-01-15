23a11f3397cba46c1d29460b26683f807430ff18
Marcel Malberg <marcel747800@gmx.de>
2013-01-15 16:56:47 +0100

    TEST
    BAM

---
+++ src/Main/MineCom.java
@@ -0,0 +1,27 @@
+package Main;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MineCom extends JavaPlugin {
+
+	
+	
+	public void OnDisable(){
+		
+		
+	}
+	
+	public void onEnable(){
+		
+		
+	}
+	
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
+		
+		
+		
+		return false;
+	}
+}
